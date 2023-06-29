1d8172a7574aace7b9140821c99a7c4d642bd9f9
ppabis <piotr@pabis.eu>
2023-06-29 18:25:56 +0200

    made bucket publicly accessible

--- infrastructure/bucket.tf
+++ infrastructure/bucket.tf
@@ -9,3 +9,32 @@ resource "random_string" "bucket-name" {
 resource "aws_s3_bucket" "bucket" {
   bucket = "photos-${random_string.bucket-name.result}"
 }
+
+resource "aws_s3_bucket_public_access_block" "bucket-public" {
+  bucket = aws_s3_bucket.bucket.id
+  block_public_policy = false
+}
+
+resource "aws_s3_bucket_website_configuration" "bucket-website" {
+  bucket = aws_s3_bucket.bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+data "aws_iam_policy_document" "bucket-policy" {
+  depends_on = [ aws_s3_bucket_public_access_block.bucket-public ]
+  statement {
+    actions = [ "s3:GetObject" ]
+    resources = [ "${aws_s3_bucket.bucket.arn}/*" ]
+    principals {
+      type = "*"
+      identifiers = [ "*" ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket-policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.bucket-policy.json
+}
