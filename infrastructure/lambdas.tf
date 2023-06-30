60831a95401b0db6d8fb935d34e70396103abaae
ppabis <piotr@pabis.eu>
2023-06-30 20:26:02 +0200

    change create lambda role and add env variables

--- infrastructure/lambdas.tf
+++ infrastructure/lambdas.tf
@@ -41,12 +41,19 @@ data "archive_file" "lambda-create" {
 resource "aws_lambda_function" "create" {
   filename = data.archive_file.lambda-create.output_path
   handler = "lambda.lambda_handler"
-  role = aws_iam_role.lambda-show-bucket.arn
+  role = aws_iam_role.lambda-post-bucket.arn
   runtime = "python3.8"
   function_name = "create-${random_string.bucket-name.result}"
   source_code_hash = data.archive_file.lambda-create.output_base64sha256
-}
 
+  environment {
+    variables = {
+      BUCKET_NAME = aws_s3_bucket.bucket.id
+      REDIRECT = "https://99j7iqgrbl.execute-api.eu-central-1.amazonaws.com/prod" # Because of Terraform dependency cycle, we cannot refer to the API Gateway here
+      # So we hardcode the value of the API Gateway URL after applying the infrastructure
+    }
+  }
+}
 resource "aws_lambda_permission" "api-create" {
   function_name = aws_lambda_function.create.function_name
   source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/POST/*"
